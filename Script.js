9ae03b03e0c79e7e1360b88f5ad6fe5f5ce8f88c
Stylex78 <112482@emmauscollege.nl>
2021-11-23 11:10:09 +0000

    DarkMode switch works, needs css

--- Script.js
+++ Script.js
@@ -1,63 +1,36 @@
 
 //Initial setup
 var ROOT = document.documentElement;
-var isDarkMode = true;
+var isDarkMode = false;
 
+var LightMode = document.styleSheets[2];
+var DarkMode = document.styleSheets[1];
+ 
 //MainSwitchColors
 function switchColors() {
     //Switch variable
-    switchVariable(isDarkMode);
-
-    //Change colors
-    SwitchSecondaryColor();
-    SwitchTersiaryColor();
-    Switch4thColor();
-    SwitchTextColor();
+    isDarkMode = !isDarkMode;
+    
+    //Change css file
+    if(isDarkMode) {
+        enableStylesheet(LightMode);
+        disableStylesheet(DarkMode);
+    } 
+    else {
+        enableStylesheet(DarkMode);
+        disableStylesheet(LightMode);
+    }
 
     //Debug
     console.log("isDarkMode = " + isDarkMode);
 }
 
-function switchVariable(Var) {
-    Var= !Var;
-
-    console.log(`Switched var to ${Var}`);
-}
-
 //SwitchColor functions
-function SwitchSecondaryColor() {
-    if(isDarkMode = false) {
-        ROOT.style.setProperty("--SecondaryColor", "rgb(40,40,40)");
-    }
-    else if(isDarkMode = true) {
-        ROOT.style.setProperty("--SecondaryColor", "rgb(215,215,215)");
-    }
+function enableStylesheet (style) {
+  style.media = '';
 }
 
-function SwitchTersiaryColor() {
-    if(isDarkMode = false) {
-        ROOT.style.setProperty("--TersiaryColor", "rgb(5,5,5)");
-    }
-    else if(isDarkMode = true) {
-        ROOT.style.setProperty("--TesiaryColor", "rgb(250,250,250)");
-    }
-}
-
-function Switch4thColor() {
-    if(isDarkMode = false) {
-        ROOT.style.setProperty("--4thColor", "rgb(20,20,20)");
-    }
-    else if(isDarkMode = true) {
-        ROOT.style.setProperty("--4thColor", "rgb(235,235,235)");
-    }
-}
-
-function SwitchTextColor() {
-    if(isDarkMode = false) {
-        ROOT.style.setProperty("--TextColor", "rgb(210,210,210)");
-    }
-    else if(isDarkMode = true) {
-        ROOT.style.setProperty("--TextColor", "rgb(45,45,45)");
-    }
+function disableStylesheet (style) {
+  style.media = 'none';
 }
 
