1ad3510fd38f518f471fffc1d5d094de7e5f5a5a
Stylex78 <112482@emmauscollege.nl>
2021-11-26 14:37:59 +0000

    Commit #69

--- Script.js
+++ Script.js
@@ -1,10 +1,15 @@
+/*
+
+DARK-/LIGHTMODE SWITCHER
+
+*/
 
 //Initial setup
 var ROOT = document.documentElement;
-var isDarkMode = false;
+var isDarkMode = true;
 
-var LightMode = document.styleSheets[2];
-var DarkMode = document.styleSheets[1];
+var LightMode = document.getElementById("LightMode");
+var DarkMode = document.getElementById("DarkMode");
  
 //MainSwitchColors
 function switchColors() {
@@ -12,7 +17,7 @@ function switchColors() {
     isDarkMode = !isDarkMode;
     
     //Change css file
-    if(isDarkMode) {
+    if(isDarkMode === false) {
         enableStylesheet(LightMode);
         disableStylesheet(DarkMode);
     } 
@@ -27,10 +32,72 @@ function switchColors() {
 
 //SwitchColor functions
 function enableStylesheet (style) {
-  style.media = '';
+    style.media = '';
 }
 
 function disableStylesheet (style) {
-  style.media = 'none';
+    style.media = 'none';
 }
 
+/*
+
+CHARACTERS V2 INFO SHOW ON HOVER
+
+
+*/
+
+//initial setup
+var lastHovered = 0;
+var ROOT = document.querySelector(':root')
+var height = document.querySelector('.CV2DivText:not(.hidden)').offsetheight;
+
+//Main Functions
+function HoverImageKnights() {
+    console.log("Hovered Knights");
+    document.getElementById('KnightsDivText').style.visibility = "visible";
+    lastHovered = 1;
+}
+
+function HoverImageVikings() {
+    console.log("Hovered Vikings")
+    document.getElementById('VikingsDivText').style.visibility = "visible";
+    lastHovered = 2;
+}
+
+function HoverImageSamurai() {
+    console.log("Hovered Samurai")
+    document.getElementById('SamuraiDivText').style.visibility = "visible";
+    lastHovered = 3;
+}
+
+function HoverImageWuLin() {
+    console.log("Hovered Wu Lin")
+    document.getElementById('WuLinDivText').style.visibility = "visible";
+    lastHovered = 4;
+}
+
+
+//Reload function
+function reloadImages() {
+    if(lastHovered !== 1) {
+    document.getElementById('KnightsDivText').style.visibility = "hidden";
+    HeightCorrectionCV2DTD();
+    }
+    if(lastHovered !== 2) {
+    document.getElementById('VikingsDivText').style.visibility = "hidden";
+    }
+    if(lastHovered !== 3) {
+    document.getElementById('SamuraiDivText').style.visibility = "hidden";
+    }
+    if(lastHovered !== 4) {
+    document.getElementById('WuLinDivText').style.visibility = "hidden";
+    }
+
+    console.log("Last Hovered = " + lastHovered)
+}
+
+//Make text get right position
+
+function HeightCorrectionCV2DTD() {
+    ROOT.style.setProperty("--CV2DivHeight", "height");
+}
