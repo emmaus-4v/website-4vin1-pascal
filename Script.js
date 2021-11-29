61a844e74e6936cf495e48c21e8f68784a5659f5
Stylex78 <112482@emmauscollege.nl>
2021-11-29 14:21:51 +0000

    Hover images works

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+
+
 /*
 
 DARK-/LIGHTMODE SWITCHER
@@ -39,6 +41,8 @@ function disableStylesheet (style) {
     style.media = 'none';
 }
 
+
+
 /*
 
 CHARACTERS V2 INFO SHOW ON HOVER
@@ -49,55 +53,79 @@ CHARACTERS V2 INFO SHOW ON HOVER
 //initial setup
 var lastHovered = 0;
 var ROOT = document.querySelector(':root')
-var height = document.querySelector('.CV2DivText:not(.hidden)').offsetheight;
+var height = String;
 
 //Main Functions
+
+//If you hover over an image, show text + change var
 function HoverImageKnights() {
     console.log("Hovered Knights");
-    document.getElementById('KnightsDivText').style.visibility = "visible";
-    lastHovered = 1;
+    document.getElementById('KnightsDivText').style.display = "block";
+    lastHovered = 10;
+
+    document.getElementById('TemporaryText').style.display = "none";
 }
 
 function HoverImageVikings() {
     console.log("Hovered Vikings")
-    document.getElementById('VikingsDivText').style.visibility = "visible";
-    lastHovered = 2;
+    document.getElementById('VikingsDivText').style.display = "block";
+    lastHovered = 7;
+
+    document.getElementById('TemporaryText').style.display = "none";
 }
 
 function HoverImageSamurai() {
     console.log("Hovered Samurai")
-    document.getElementById('SamuraiDivText').style.visibility = "visible";
-    lastHovered = 3;
+    document.getElementById('SamuraiDivText').style.display = "block";
+    lastHovered = 8;
+
+    document.getElementById('TemporaryText').style.display = "none";
 }
 
 function HoverImageWuLin() {
     console.log("Hovered Wu Lin")
-    document.getElementById('WuLinDivText').style.visibility = "visible";
-    lastHovered = 4;
+    document.getElementById('WuLinDivText').style.display = "block";
+    lastHovered = 5;
+
+    document.getElementById('TemporaryText').style.display = "none";
 }
 
 
 //Reload function
 function reloadImages() {
-    if(lastHovered !== 1) {
-    document.getElementById('KnightsDivText').style.visibility = "hidden";
-    HeightCorrectionCV2DTD();
+
+    //If lasthovered isnt an image, hide the text of image
+    if(lastHovered !== 10) {
+    document.getElementById('KnightsDivText').style.display = "none";
+
     }
-    if(lastHovered !== 2) {
-    document.getElementById('VikingsDivText').style.visibility = "hidden";
+
+    if(lastHovered !== 7) {
+    document.getElementById('VikingsDivText').style.display = "none";
     }
-    if(lastHovered !== 3) {
-    document.getElementById('SamuraiDivText').style.visibility = "hidden";
+
+    if(lastHovered !== 8) {
+    document.getElementById('SamuraiDivText').style.display = "none";
     }
-    if(lastHovered !== 4) {
-    document.getElementById('WuLinDivText').style.visibility = "hidden";
+
+    if(lastHovered !== 5) {
+    document.getElementById('WuLinDivText').style.display = "none";
     }
 
     console.log("Last Hovered = " + lastHovered)
-}
+    //HeightCorrectionCV2DTD();
 
-//Make text get right position
+    //1 Row pixels * amount of rows
+    height = ((37 * lastHovered) + 70)
+        if(lastHovered == 10) {
+            height = height - 20;
+        }
 
-function HeightCorrectionCV2DTD() {
-    ROOT.style.setProperty("--CV2DivHeight", "height");
+    //COnvert to useable string
+    height = height + "px";
+
+    console.log("Div height = " + height);
+
+    //Change height of div
+    document.getElementById('CV2DivTextDiv').style.height = height;
 }
